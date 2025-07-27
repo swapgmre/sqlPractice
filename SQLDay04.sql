ca60fa44347aa5e334067c675eeb0fc1f76c5ae2
Swapnil <swapgmre@gmail.com>
2025-07-27 20:52:18 +0530

    Update Null Functions

--- SQLDay04.sql
+++ SQLDay04.sql
@@ -491,3 +491,81 @@ FROM
 	SELECT '2025-08'
 ) t
 -- WHERE ISDATE(OrderDate) = 0
+
+
+-- NULL vs Empty String vs Blank Space
+-- NULL - means nothing, unknown
+-- Empty String - String value has zero characters
+-- Blank Space - String value has one or more space characters
+
+WITH Orders AS (
+SELECT 1 Id, 'A' Category UNION
+SELECT 2, NULL UNION
+SELECT 3, '' UNION
+SELECT 4, '  '
+)
+SELECT
+*,
+DATALENGTH (Category) CategoryLen
+FROM Orders
+
+-- HANDLING NULLS
+-- DATA POLICIES - Set of rules that defines how data should be handled.
+-- We can define it like below
+-- Only use NULLs and empty strings, but avoid blank spaces.
+
+WITH Orders AS (
+SELECT 1 Id, 'A' Category UNION
+SELECT 2, NULL UNION
+SELECT 3, '' UNION
+SELECT 4, '  '
+)
+SELECT
+*,
+TRIM(Category) Policy1
+FROM Orders
+
+-- Check with DATALENGTH if it has removed the spaces
+-- TRIM - remove unwanted leading and trailing spaces from a string
+
+-- Option 2
+-- Only use NULLS and avoid using empty string and blank spaces
+
+WITH Orders AS (
+SELECT 1 Id, 'A' Category UNION
+SELECT 2, NULL UNION
+SELECT 3, '' UNION
+SELECT 4, '  '
+)
+SELECT
+*,
+TRIM(Category) Policy1,
+NULLIF(TRIM(Category), '') Policy2,
+COALESCE(NULLIF(TRIM(Category), ''), 'unknown') Policy3
+FROM Orders
+
+-- Option 3 
+-- Use the default value 'unknown' and avoid using nulls, empty strings, and blank spaces.
+
+-- When to use which options
+
+-- Option 2
+-- Replacing empty strings and blanks with NULL during data preparation
+-- before inserting	into a database to optimize storage and performance.
+
+-- Option 3
+-- Replacing empty strings, blanks, NULL with default value
+-- during data preparation before using it in reporting to 
+-- improve readiblity and reduce confusion
+
+-- NULL Functions Summary
+-- Nulls are special markers means missing value
+-- Using Nulls can optimize storage and performance
+
+-- Use Cases
+-- Handle Nulls - Data Aggregation
+-- Handle Nulls - Mathematical Operations
+-- Handle Nulls - Joining Tables
+-- Handle Nulls - Sorting Data
+-- Finding unmatched dataa - Left Anti Join
+-- Data Policies - Nulls and Default Value
